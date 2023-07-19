2efc3adad5ddf957430ea5bef7f550e0feaca624
kento0720 <96482941+kento0720@users.noreply.github.com>
2023-07-19 18:01:24 +0900

    Commit from TeleportHQ

--- src/components/frame.vue
+++ src/components/frame.vue
@@ -17,7 +17,7 @@
           <div class="frame-frame6">
             <img
               alt="imagemessagekiri15011"
-              src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/7689dac6-b458-4b2a-aa5a-1a4307c8400c/13f461e0-2016-4218-a21a-0a653a6a7180?org_if_sml=1585284"
+              src="/external/imagemessagekiri15011-r2o2-800w.png"
               class="frame-imagemessagekiri1"
             />
             <div class="frame-frame40">
@@ -39,7 +39,7 @@
                 <span>
                   <span>
                     私たちの目の前に広がる未来への道筋を、
-                    <span v-html="rawt0pr"></span>
+                    <span v-html="rawxn4l"></span>
                   </span>
                   <br />
                   <span>
@@ -51,12 +51,12 @@
                 <span>
                   <span>
                     これからの世界は、想像を超えたスピードで進化し続けます。
-                    <span v-html="rawpqd1"></span>
+                    <span v-html="raw9zfs"></span>
                   </span>
                   <br />
                   <span>
                     その変化を見据え、新たな課題やニーズに応えるための
-                    <span v-html="rawnscq"></span>
+                    <span v-html="rawhhla"></span>
                   </span>
                   <br />
                   <span>革新的なソリューションやプロダクトを創造します。</span>
@@ -72,6 +72,78 @@
         </div>
       </div>
     </div>
+    <div class="frame-frame382">
+      <div class="frame-frame371">
+        <div class="frame-frame351">
+          <div class="frame-frame341">
+            <span class="frame-text29"><span>Design for the Future</span></span>
+            <span class="frame-text31">
+              <span>未来志向デザインでビジョンを現実に</span>
+            </span>
+          </div>
+        </div>
+      </div>
+      <span class="frame-text33"><span>Scroll Down</span></span>
+      <div class="frame-willver1">
+        <div class="frame-frame391">
+          <div class="frame-frame61">
+            <img
+              alt="imagemessagekiri15011"
+              src="/external/imagemessagekiri15011-r2o2-800w.png"
+              class="frame-imagemessagekiri11"
+            />
+            <div class="frame-frame401">
+              <span class="frame-text35">
+                <span>WILLVERが目指す未来のカタチ</span>
+              </span>
+              <span class="frame-text37">
+                <span>
+                  <span>時代が刻々と変化する中で、</span>
+                  <br />
+                  <span>
+                    未来の社会と人々の暮らしにどのような価値を提供できるのか。
+                  </span>
+                  <br />
+                  <span>WILLVERはその答えを追求し続けています。</span>
+                </span>
+              </span>
+              <span class="frame-text44">
+                <span>
+                  <span>
+                    私たちの目の前に広がる未来への道筋を、
+                    <span v-html="raw3ds1"></span>
+                  </span>
+                  <br />
+                  <span>
+                    創造力と独自の発想で照らし出すことが私たちの使命です。
+                  </span>
+                </span>
+              </span>
+              <span class="frame-text49">
+                <span>
+                  <span>
+                    これからの世界は、想像を超えたスピードで進化し続けます。
+                    <span v-html="rawbrvb"></span>
+                  </span>
+                  <br />
+                  <span>
+                    その変化を見据え、新たな課題やニーズに応えるための
+                    <span v-html="raw2ubf"></span>
+                  </span>
+                  <br />
+                  <span>革新的なソリューションやプロダクトを創造します。</span>
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="frame-frame2">
+        <div class="frame-frame383">
+          <span class="frame-text56"><span>Scroll Down</span></span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -81,9 +153,12 @@ export default {
   props: {},
   data() {
     return {
-      rawt0pr: ' ',
-      rawpqd1: ' ',
-      rawnscq: ' ',
+      rawxn4l: ' ',
+      raw9zfs: ' ',
+      rawhhla: ' ',
+      raw3ds1: ' ',
+      rawbrvb: ' ',
+      raw2ubf: ' ',
     }
   },
 }
@@ -292,6 +367,207 @@ export default {
   justify-content: center;
 }
 .frame-text27 {
+  color: rgba(53, 217, 217, 1);
+  height: auto;
+  font-size: 18px;
+  font-style: Medium;
+  text-align: left;
+  font-family: Barlow;
+  font-weight: 500;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.frame-frame382 {
+  gap: 16px;
+  display: flex;
+  padding: 1px 0;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+.frame-frame371 {
+  gap: 100px;
+  height: 780px;
+  display: flex;
+  padding: 0 8px;
+  align-items: center;
+  flex-shrink: 0;
+  flex-direction: column;
+  justify-content: center;
+}
+.frame-frame351 {
+  gap: 10px;
+  width: 1478px;
+  height: 252px;
+  display: flex;
+  padding: 0 49px;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+}
+.frame-frame341 {
+  gap: 10px;
+  top: 0px;
+  left: 0px;
+  width: 1474px;
+  display: flex;
+  padding: 7px 47px;
+  position: absolute;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+.frame-text29 {
+  color: rgba(232, 233, 237, 1);
+  width: 1380px;
+  height: auto;
+  font-size: 136px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) ;
+  font-style: italic;
+  text-align: left;
+  font-family: Poppins;
+  font-weight: 700;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.frame-text31 {
+  color: rgba(232, 233, 237, 1);
+  height: auto;
+  font-size: 24px;
+  font-style: Bold;
+  text-align: left;
+  font-family: Noto Sans JP;
+  font-weight: 700;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.frame-text33 {
+  color: rgba(53, 217, 217, 1);
+  height: auto;
+  font-size: 18px;
+  font-style: Medium;
+  text-align: left;
+  font-family: Barlow;
+  font-weight: 500;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.frame-willver1 {
+  width: 100%;
+  height: 943px;
+  display: flex;
+  position: relative;
+  align-items: flex-start;
+  flex-shrink: 0;
+}
+.frame-frame391 {
+  gap: 1px;
+  top: 0.5px;
+  left: 0px;
+  width: 1478px;
+  display: flex;
+  position: absolute;
+  align-items: center;
+  flex-shrink: 0;
+  background-color: rgba(13, 24, 59, 1);
+}
+.frame-frame61 {
+  gap: 10px;
+  display: flex;
+  padding: 173px 85px;
+  position: relative;
+  align-items: flex-start;
+  flex-direction: column;
+}
+.frame-imagemessagekiri11 {
+  right: -559px;
+  width: 732px;
+  bottom: -18px;
+  height: 943px;
+  position: absolute;
+}
+.frame-frame401 {
+  gap: 67px;
+  height: 596px;
+  display: flex;
+  z-index: 1;
+  align-items: flex-start;
+  flex-shrink: 0;
+  flex-direction: column;
+}
+.frame-text35 {
+  color: rgba(40, 112, 229, 1);
+  width: 759px;
+  height: auto;
+  font-size: 53px;
+  font-style: Bold;
+  text-align: left;
+  font-family: Noto Sans JP;
+  font-weight: 700;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.frame-text37 {
+  color: rgba(255, 255, 255, 1);
+  height: auto;
+  font-size: 22px;
+  font-style: Regular;
+  text-align: left;
+  font-family: Noto Sans JP;
+  font-weight: 400;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.frame-text44 {
+  color: rgba(255, 255, 255, 1);
+  height: auto;
+  font-size: 22px;
+  font-style: Regular;
+  text-align: left;
+  font-family: Noto Sans JP;
+  font-weight: 400;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.frame-text49 {
+  color: rgba(255, 255, 255, 1);
+  height: auto;
+  font-size: 22px;
+  font-style: Regular;
+  text-align: left;
+  font-family: Noto Sans JP;
+  font-weight: 400;
+  line-height: normal;
+  font-stretch: normal;
+  text-decoration: none;
+}
+.frame-frame2 {
+  gap: 10px;
+  width: 100%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  flex-shrink: 0;
+  flex-direction: column;
+  justify-content: center;
+  background-color: rgba(217, 217, 217, 1);
+}
+.frame-frame383 {
+  gap: 16px;
+  display: flex;
+  padding: 1px 0;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+.frame-text56 {
   color: rgba(53, 217, 217, 1);
   height: auto;
   font-size: 18px;
